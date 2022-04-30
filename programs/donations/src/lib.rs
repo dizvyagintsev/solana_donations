aee6f984daab89edfa41f8d40f94e4e4534451c9
Denis Zvyagintsev <dizvyagintsev@avito.ru>
2022-04-30 19:12:10 +0300

    add `create_fundraising` handler

--- programs/donations/src/lib.rs
+++ programs/donations/src/lib.rs
@@ -6,10 +6,26 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod donations {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_fundraising(ctx: Context<Fundraising>, authority: Pubkey) -> Result<()> {
+        let wallet = &mut ctx.accounts.wallet;
+        wallet.bump = *ctx.bumps.get("wallet").unwrap();
+        wallet.authority = authority;
         Ok(())
     }
 }
 
+#[account]
+pub struct Wallet {
+    bump: u8,
+    authority: Pubkey,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Fundraising<'info> {
+    // TODO: compute space
+    #[account(init, payer=user, space=1000, seeds=[b"wallet", user.key().as_ref()], bump)]
+    pub wallet: Account<'info, Wallet>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
--- tests/donations.ts
+++ tests/donations.ts
@@ -1,16 +1,30 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Donations } from "../target/types/donations";
+import { PublicKey } from '@solana/web3.js';
 
 describe("donations", () => {
+  const provider = anchor.AnchorProvider.env();
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Donations as Program<Donations>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const [walletPDA, _] = await PublicKey
+        .findProgramAddress(
+            [
+              anchor.utils.bytes.utf8.encode("wallet"),
+              provider.wallet.publicKey.toBuffer()
+            ],
+            program.programId
+        );
+
+    const tx = await program.methods.createFundraising(provider.wallet.publicKey).accounts({
+      user: provider.wallet.publicKey,
+      wallet: walletPDA,
+    }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
